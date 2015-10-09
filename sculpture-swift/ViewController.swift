6230ad6ed8be47280e633bb27ba02e72a69a16af
Canna Wen <cannawen@gmail.com>
2015-10-09 18:00:16 -0400

    Initial Commit

---
+++ sculpture-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sculpture-swift
+//
+//  Created by Canna Wen on 2015-10-09.
+//  Copyright (c) 2015 Canna Wen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
