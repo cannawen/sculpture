883e8f4b770093a35bce120193d40f7f731efd02
Canna Wen <cannawen@gmail.com>
2015-10-16 20:19:28 -0400

    Added camera button on home screen [#105269350]

---
+++ sculpture-swift/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  sculpture-swift
+//
+//  Created by Canna Wen on 2015-10-09.
+//  Copyright (c) 2015 Canna Wen. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+
+    @IBAction func selectedCamera(sender: UIBarButtonItem) {
+        
+    }
+    
+}
+
